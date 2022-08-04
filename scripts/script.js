712852f7b12ab782cdf32c1edc507cbbda6f2e4e
Ran Ren <ranforrestren@gmail.com>
2022-08-04 19:30:52 -0400

    add function for creating DOM element with new book

--- scripts/script.js
+++ scripts/script.js
@@ -6,10 +6,10 @@ submitButton.addEventListener('click', (e) => { addBookToLibrary(); });
 let myLibrary = [];
 
 // constructor for book objects
-function Book(author, title, date, isRead) {
+function Book(author, title, pages, isRead) {
   this.author = author;
   this.title = title;
-  this.date = date;
+  this.pages = pages;
   this.isRead = isRead;
 }
 
@@ -24,4 +24,35 @@ function addBookToLibrary() {
   const book = new Book(author.value, title.value, pages.value, isRead.value);
   myLibrary.push(book);
   console.log(myLibrary);
+
+  // update library with new book
+  updateLibrary();
+}
+
+// add HTML elements from Library to page
+function updateLibrary() {
+  // find HTML element to put newly created books into
+  const library = document.querySelector('#library');
+
+  for (const book of myLibrary) {
+    // create HTML elements
+    const newBook = document.createElement('div');
+    const bookAuthor = document.createElement('div');
+    const bookTitle = document.createElement('div');
+    const bookPages = document.createElement('div');
+    const bookIsRead = document.createElement('div');
+
+    // add values from book object to elements
+    bookAuthor.textContent = book.author;
+    bookTitle.textContent = book.title;
+    bookPages.textContent = book.pages;
+    bookIsRead.textContent = book.isRead;
+
+    // add HTML elements to DOM
+    library.appendChild(newBook);
+    newBook.appendChild(bookAuthor);
+    newBook.appendChild(bookTitle);
+    newBook.appendChild(bookPages);
+    newBook.appendChild(bookIsRead);
+  }
 }
